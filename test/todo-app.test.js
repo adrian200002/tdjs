58a37c695dbc8ccd77ad84eaddc55e90b2bbc943
Adrian <adrian.krause.ak@gmail.com>
2024-06-04 18:17:36 +0200

    copied app styles from todo mvc

--- test/todo-app.test.js
+++ test/todo-app.test.js
@@ -4,4 +4,4 @@ const path = require('path');
 const html = fs.readFileSync(path.resolve(__dirname, '../index.html'));
 require('jsdom-global')(html);
 const app = require('../lib/todo-app.js');
-const id = 'test-app';   
+const id = 'test-app';
