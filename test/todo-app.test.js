7f8a112e25370702bcd736c0fae18658f6ee77b6
Adrian <adrian.krause.ak@gmail.com>
2024-06-04 22:32:22 +0200

    added tests and toggle case to update function

--- test/todo-app.test.js
+++ test/todo-app.test.js
@@ -29,3 +29,18 @@ test('add new todo to model.todos Array via update', function(t) {
     t.deepEqual(modified_model.todos[0], expected, 'todo list item added');
     t.end();
 })
+
+test('Toggle todo item from done=false to done=true', function(t) {
+    const model = JSON.parse(JSON.stringify(app.model));
+    const model_with_todo = app.update('ADD', model, 'toggle todo');
+    const item = model_with_todo.todos[0];
+    const model_todo_done = app.update('TOGGLE', model_with_todo, item.id);
+    const expected = {id: 1, title: 'toggle todo', done: true };
+    t.deepEqual(model_todo_done.todos[0], expected);
+    const model_second_item = app.update('ADD', model_todo_done, 'toggle todo2');
+    t.equal(model_second_item.todos.length, 2, 'model should have 2 todo items');
+    const model_todo_undone = app.update('TOGGLE', model_second_item, item.id);
+    const undone = {id: 1, title: 'toggle todo', done: false };
+    t.deepEqual(model_todo_undone.todos[0], undone);
+    t.end();
+})
