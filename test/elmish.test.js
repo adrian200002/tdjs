11e87bd3f982eb9531a7a04a84081a780f7c45f4
Adrian <adrian.krause.ak@gmail.com>
2024-06-06 00:52:01 +0200

    added new tests and created mount, add_attributes functions

--- test/elmish.test.js
+++ test/elmish.test.js
@@ -23,3 +23,47 @@ test('elmish.empty function should empty the content of node', function(t) {
     t.equal(exampleNode.childNodes.length, 0);
     t.end();
 })
+
+test('elmish.add_attributes applies class attribute to a node', function(t) {
+    const root = document.createElement(id);
+    let div = document.createElement('div');
+    div.id = 'divid';
+    div = elmish.add_attributes(['class=sss'], div);
+    root.appendChild(div);
+    const nodes = root.getElementsByClassName('sss');
+    t.equal(nodes.length, 1, 'div class applied');
+    t.end();
+})
+
+test('elmish.add_atributes applies placeholder on input element', function(t) {
+    const root = document.getElementById(id);
+    let input = document.createElement('input');
+    input.id = 'new-todo';
+    input = elmish.add_attributes(['placeholder=What needs to be done?'], input);
+    root.appendChild(input);
+    const placeholder = document.getElementById('new-todo').placeholder;
+    t.equal(placeholder, 'What needs to be done?');
+    t.end();
+})
+
+test('elmish.add_atributes should not add unvalid attribute', function(t) {
+    let div = document.createElement('div');
+    div.id = 'divid';
+    const clone = div.cloneNode(true);
+    
+    div = elmish.add_attributes(['unknown=as'], div);
+    t.deepEqual(clone, div, "<div> has not been altered");
+    t.end();
+})
+
+test.only('elmish.add_atributes should add autofocus attribute on input', function(t) {
+    document.getElementById(id).appendChild(
+        elmish.add_attributes(['class=todo', 'autofocus', 'id=new'],
+        document.createElement('input')
+        )
+    )
+
+    t.equal(document.getElementById('new'), document.activeElement);
+    elmish.empty(document)
+    t.end();
+})
